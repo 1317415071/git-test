91a0b9ebd9d5fe3d2aa669ce7e3f362be20c65c9
Li <1317415071@qq.com>
2022-04-19 09:59:20 +0800

    commit by ll

--- src/main/java/com/ll/GitTest02.java
+++ src/main/java/com/ll/GitTest02.java
@@ -6,5 +6,6 @@ public class GitTest02 {
         System.out.println("Hello Git1!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
+        System.out.println("Hello Git4!");
     }
 }
