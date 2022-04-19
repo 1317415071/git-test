92da622f904b40dc5bc041864e365fd54ae4ec35
1317415071 <68584145+1317415071@users.noreply.github.com>
2022-04-19 09:30:27 +0800

    Update GitTest02.java

--- src/main/java/com/ll/GitTest02.java
+++ src/main/java/com/ll/GitTest02.java
@@ -3,5 +3,6 @@ package com.ll;
 public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git1!");
     }
 }
